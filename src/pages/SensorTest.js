fc7d2b6b9e9ce3071110489b83a0d8d1aec4ff19
Sun Chen <chen.shenglun@gmail.com>
2021-06-19 14:18:04 +0800

    add acceleration

--- src/pages/SensorTest.js
+++ src/pages/SensorTest.js
@@ -5,6 +5,7 @@ import s from './SensorTest.module.sass'
 function SensorTestPage() {
 
 	const [stats, setStats] = useState(null)
+	const [accel, setAccel] = useState({ x: 0, y: 0, z: 0 })
 
 	useEffect(() => {
 		// let video = document.createElement('video')
@@ -33,11 +34,14 @@ function SensorTestPage() {
 	}
 
 	function bindDeviceSensors() {
+		window.addEventListener('devicemotion', (event) => {
+			setAccel({ x: event.acceleration.x, y: event.acceleration.y, z: event.acceleration.z })
+		})
 		window.addEventListener('deviceorientation',(event) => {
 			// Expose each orientation angle in a more readable way
-			let rotation_degrees = event.alpha;
-			let frontToBack_degrees = event.beta;
-			let leftToRight_degrees = event.gamma;
+			let rotation_degrees = event.alpha
+			let frontToBack_degrees = event.beta
+			let leftToRight_degrees = event.gamma
 			setStats(rotation_degrees + ' ' + frontToBack_degrees + ' ' + leftToRight_degrees)
 
 			// // Update velocity according to how tilted the phone is
@@ -72,6 +76,12 @@ function SensorTestPage() {
 				<div onClick={getPermissions}>Dev Stats:</div>
 				<div>
 					{stats}
+				</div>
+				<div>
+					<div>Motion</div>
+					<div>x: 0</div>
+					<div>y: 0</div>
+					<div>z: 0</div>
 				</div>
 			</div>
 		</div>
