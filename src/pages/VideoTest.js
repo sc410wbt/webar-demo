7037c892a07890bc5301c9664ccc37df7c39f92f
Sun Chen <chen.shenglun@gmail.com>
2021-06-22 18:15:25 +0800

    working video test with threejs video example

--- src/pages/VideoTest.js
+++ src/pages/VideoTest.js
@@ -1,12 +1,16 @@
 import React, {useState, useEffect} from 'react'
 import * as THREE from 'three'
+import {OrbitControls} from '../OrbitControls'
+
+import exampleSrc from '../textures/pano.mp4'
 
 import s from './VideoTest.module.sass'
 
 const scene = new THREE.Scene()
 const renderer = new THREE.WebGLRenderer( { antialias: true })
 let camera
-// let controls
+let controls
+let video
 
 function VideoTestPage() {
 
@@ -17,20 +21,20 @@ function VideoTestPage() {
 		if (appWrapper.children.length <= 0) appWrapper.appendChild(renderer.domElement)
 
 		camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 300)
-		camera.position.set(0,0,10)
+		camera.position.set(0,0,0.01)
 		renderer.setClearColor(0x030303, 1)
 		renderer.setPixelRatio(1.5) //window.devicePixelRatio)
 		renderer.setSize(window.innerWidth, window.innerHeight)
 		renderer.shadowMap.enabled = true
 		renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
-		// controls = new OrbitControls(camera, renderer.domElement)
+		controls = new OrbitControls(camera, renderer.domElement)
 		// controls.maxPolarAngle = Math.PI / 2 + 0.15
 		// controls.minPolarAngle = Math.PI / 2 - 0.15
 		// controls.maxAzimuthAngle = 0.7
 		// controls.minAzimuthAngle = -0.7
-		// controls.enableZoom = false
-		// controls.enableDamping = true
+		controls.enableZoom = false
+		controls.enableDamping = true
 		// controls.dampingFactor = 0.12
 		// controls.rotateSpeed *= 0.4
 
@@ -38,19 +42,45 @@ function VideoTestPage() {
 		light()
 		animate()
 
+		setTimeout(() => {
+			video.play()
+		}, 1000)
+
 	}, [])
 
 	function populate() {
-		let cubeG = new THREE.BoxBufferGeometry(1, 1, 1)
-		let cubeM = new THREE.MeshBasicMaterial({ color: 0xFFFFFF })
-		let cube = new THREE.Mesh(cubeG, cubeM)
-		cube.position.set(0, 2, 0)
-		scene.add(cube)
+		// let cubeG = new THREE.BoxBufferGeometry(1, 1, 1)
+		// let cubeM = new THREE.MeshBasicMaterial({ color: 0xFFFFFF })
+		// let cube = new THREE.Mesh(cubeG, cubeM)
+		// cube.position.set(0, 2, 0)
+		// scene.add(cube)
+
+		video = document.createElement('video')
+
+		video.src = exampleSrc
+		video.loop = true
+		video.preload = 'auto'
+		video.playsInline = true
+		video.muted = true
+		video.autoplay = true
+		// video.poster = poster
+
+		let videoTexture = new THREE.VideoTexture(video)
+		videoTexture.needsUpdate = true
+		videoTexture.minFilter = THREE.LinearFilter
+		videoTexture.magFilter = THREE.LinearFilter
+		videoTexture.wrapS = THREE.RepeatWrapping
+		videoTexture.wrapT = THREE.RepeatWrapping
+		// videoTexture.flipY = true
+		// videoTexture.repeat.set(1.01, 1) // no negative values for iOS
+		// videoTexture.offset.set(-0.1, 0)
+		videoTexture.format = THREE.RGBFormat
+		videoTexture.crossOrigin = 'anonymous'
 
 		let sphereG = new THREE.SphereBufferGeometry(1, 40, 40)
-		let sphereM = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, side: THREE.DoubleSide })
+		let sphereM = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, side: THREE.DoubleSide, map: videoTexture })
 		let sphere = new THREE.Mesh(sphereG, sphereM)
-		sphere.position.set(0, 0, 10)
+		sphere.position.set(0, 0, 0)
 		scene.add(sphere)
 	}
 
